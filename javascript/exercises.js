4108a7cc914d70d1b3dd6eaa2dc0db57ab50465a
Conner Shumway <ckshum16@stlawu.edu>
2020-01-29 14:12:02 -0500

    exercise 3

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -1,18 +1,29 @@
 // JavaScript Syntax Exercises
 
 // 1. print the sum of the squares of the integers from 1 through 100
-let sum = 0
+let sum = 0;
 for (let i = 1; i <= 10; i++) {
-  sum += (i ** 2)
+  sum += (i ** 2);
 }
-console.log(sum)
+console.log(sum);
 
 // 2. Print the first fibonacci number that is greater than 1000
-let fib1 = 1
-let fib2 = 1
+let fib1 = 1;
+let fib2 = 1;
 while (fib2 < 1000) {
-  let temp = fib1
-  fib1 = fib2
-  fib2 = temp + fib2
+  let temp = fib1;
+  fib1 = fib2;
+  fib2 = temp + fib2;
 }
-console.log(fib2)
+console.log(fib2);
+
+// 3. Print the number of integers from 1 through 100 that are divisible by 3 or 5 but not both
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0) && (i % 5 === 0) {
+    continue;
+  } else if (i % 3) {
+    console.log(i);
+  } else if (i % 5) {
+    console.log(i);
+  }
+}
