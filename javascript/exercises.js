391970415df5a9b537e74fa817aefdbfada44e8f
Conner Shumway <ckshum16@stlawu.edu>
2020-01-29 14:06:15 -0500

    exercise 2

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -6,3 +6,13 @@ for (let i = 1; i <= 10; i++) {
   sum += (i ** 2)
 }
 console.log(sum)
+
+// 2. Print the first fibonacci number that is greater than 1000
+let fib1 = 1
+let fib2 = 1
+while (fib2 < 1000) {
+  let temp = fib1
+  fib1 = fib2
+  fib2 = temp + fib2
+}
+console.log(fib2)
