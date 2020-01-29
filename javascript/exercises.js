0a613513fe6f895cce1e9956563c41e9d07973c0
Conner Shumway <ckshum16@stlawu.edu>
2020-01-29 13:55:47 -0500

    exercise 1

---
+++ javascript/exercises.js
@@ -0,0 +1,8 @@
+// JavaScript Syntax Exercises
+
+// 1. print the sum of the squares of the integers from 1 through 100
+let sum = 0
+for (let i = 1; x <= 10; i++) {
+  sum = sum + (i ** 2)
+}
+console.log(sum)
