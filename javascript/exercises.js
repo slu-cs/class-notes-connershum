f905e11be3280086d07c7edf011dbc7bb9fddb37
Conner Shumway <ckshum16@stlawu.edu>
2020-01-29 13:57:35 -0500

    1 change

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -2,7 +2,7 @@
 
 // 1. print the sum of the squares of the integers from 1 through 100
 let sum = 0
-for (let i = 1; x <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
   sum = sum + (i ** 2)
 }
 console.log(sum)
