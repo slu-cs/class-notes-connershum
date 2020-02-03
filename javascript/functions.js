cbecda7730e69490db2de183604604b1f460cc45
Conner Shumway <ckshum16@stlawu.edu>
2020-02-03 13:24:22 -0500

    Javascript functions

---
+++ javascript/functions.js
@@ -0,0 +1,20 @@
+// Function Syntax
+
+// Function Definition
+const avg = function(a, b) {
+  return (a + b) / 2;
+}
+
+// Function call
+console.log(avg(5, 6));
+
+// another Definition
+const print = function(a,b) {
+  console.log(`a=${a}, b=${b}`)
+};
+
+// Undefined values
+const result = print(7, 8);
+console.log(result);
+print(9);
+print();
