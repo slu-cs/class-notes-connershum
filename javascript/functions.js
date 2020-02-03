b6effec7e867df9c022aea54895d4937f5d501f4
Conner Shumway <ckshum16@stlawu.edu>
2020-02-03 13:34:02 -0500

    javascript constructors

--- javascript/functions.js
+++ javascript/functions.js
@@ -3,7 +3,7 @@
 // Function Definition
 const avg = function(a, b) {
   return (a + b) / 2;
-}
+};
 
 // Function call
 console.log(avg(5, 6));
@@ -18,3 +18,34 @@ const result = print(7, 8);
 console.log(result);
 print(9);
 print();
+
+// Object literal with a method
+const rectangle = {
+  width: 10,
+  height: 20,
+  area: function() {
+    return this.width * this.height;
+  }
+};
+
+// Method call
+console.log(rectangle.area());
+
+// Construct Definition (similar to a class)
+const Rectangle = function(width, height) {
+  this.width = width;
+  this.height = height;
+};
+
+// Shared Method
+Rectangle.prototype.area = function() {
+  return this.width * this.height;
+};
+
+// Constructing Objects
+const small = new Rectangle(1, 2);
+const large = new Rectangle(10, 20);
+
+// Method calls
+console.log(small.area());
+console.log(large.area());
