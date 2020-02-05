3006b699ec7b53c358e455c934cd61cb3d1faf3b
Conner Shumway <ckshum16@stlawu.edu>
2020-02-05 14:18:28 -0500

    exercise 1

--- javascript/Feb5Exercises.js
+++ javascript/Feb5Exercises.js
@@ -1,7 +1,21 @@
 // Syntax to know
-// JavaScript arrays have a filter method that works like this:
+
+// 1. JavaScript arrays have a filter method that works like this:
 const numbers = [1, 2, 3, 4, 5];
 const odds = numbers.filter(x => x%2 > 0);
-
 console.log(numbers);
 console.log(odds);
+
+// Try it out, and then write your own filter function, which would be called differently:
+const filter = function(list, filt) {
+  const copy = [];
+  for (const element of array) {
+    if (filt(element) === true){
+      copy.push(element);
+    }
+  }
+  return copy;
+};
+
+const numbers = [1, 2, 3, 4, 5];
+const odds = filter(numbers, x => x%2 > 0);
