c0f46822d7437fc93e64583230316e13c9148627
Conner Shumway <ckshum16@stlawu.edu>
2020-02-26 12:07:25 -0500

    q

--- mongo/quiz.js
+++ mongo/quiz.js
@@ -64,10 +64,11 @@ const question = function(prompt) {
 
 // B. Make small talk again, using promises.
 question('What is your name? ')
-  .then(resolve => console.log('Hello', resolve, '.'))
+  .then(resolve => console.log('Hello', resolve, '.') {
   question('How are you doing? ')
     .then(resolve2 => console.log('I am also', resolve2, '.'))
     .catch(error => console.error(error.stack));
+  });
 
 //////////////////////////////////////////////////////////////// Question 4
 // Question 4 is commented out because otherwise it would interfere with Question 3.
