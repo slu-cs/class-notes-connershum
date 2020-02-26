818ca7346c5422f310523a0326ac63fa046f4eba
Conner Shumway <ckshum16@stlawu.edu>
2020-02-25 19:09:51 -0500

    q

--- mongo/quiz.js
+++ mongo/quiz.js
@@ -9,11 +9,11 @@ const labs = [
 ];
 
 // A. Generate a new array containing the seat numbers of all the labs (by invoking the built-in map method).
-const labs2 = labs;
-console.log(labs2.map(l => l.seats));
+const labs2 = labs.map(l => l.seats);
+console.log(labs2);
 
 // B. Generate a new array containing only the labs with more than 10 seats (by invoking the built-in filter method).
-const labs3 = filter(labs, x => x.seats > 10)
+const labs3 = labs.filter(x => x.seats > 10);
 console.log(labs3);
 
 // C. Sort the existing array in order of (increasing) seat numbers (by invoking the built-in sort method).
