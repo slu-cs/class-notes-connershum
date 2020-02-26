226baebcb3a64644cd31474aa6a551173154081b
Conner Shumway <ckshum16@stlawu.edu>
2020-02-26 09:34:57 -0500

    q

--- mongo/quiz.js
+++ mongo/quiz.js
@@ -42,9 +42,9 @@ const user = readline.createInterface({
 
 // A. Make small talk, using traditional callbacks.
 user.question('What is your name? ', function(name) {
-  console.log('Hello ', name);
+  console.log('Hello', name, '.');
   user.question('How are you doing? ', function(feeling) {
-    console.log('I am also ', feeling);
+    console.log('I am also', feeling, '.');
   });
 });
 
