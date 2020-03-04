055375bd934fad960c7a9fc6cf61883eecdacc41
Conner Shumway <ckshum16@stlawu.edu>
2020-03-04 13:37:38 -0500

    add ons

--- express/hello/app.js
+++ express/hello/app.js
@@ -4,9 +4,29 @@ const express = require('express');
 // Create the server
 const app = express();
 
-// Respond to one request
+// cs-linuxlab-39.stlawu.edu:3000/
 app.get('/', function(request, response) {
   response.send('Hello Conner');
+});
+
+// cs-linuxlab-39.stlawu.edu:3000/foo
+app.get('/foo', function(request, response) {
+  response.send('Hello foo')
+});
+
+// cs-linuxlab-39.stlawu.edu:3000/foo/bar
+app.get('/foo/bar', function(request, response) {
+  response.send('Hello bar')
+});
+
+// cs-linuxlab-09.stlawu.edu:3000/zap/z (for any z)
+app.get('/zap/:z', function(request, response) {
+  response.send(`Hello ${request.params.z}`);
+});
+
+// cs-linuxlab-09.stlawu.edu:3000/zap?x=&y=
+app.get('/zap', function(request, response) {
+  response.send(`Hello zap with ${request.query.x} and ${request.query.y}`);
 });
 
 // Start the server
