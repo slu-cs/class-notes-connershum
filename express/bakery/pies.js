2faa8117bc84a36490d6981d6de69307f18297b0
Conner Shumway <ckshum16@stlawu.edu>
2020-03-09 14:17:30 -0400

    fix array name

--- express/bakery/pies.js
+++ express/bakery/pies.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 // Pretend database collection
-const cakes = [
+const pies = [
   {id: 'apple', flavor: 'Apple'},
   {id: 'cherry', flavor: 'Cherry'}
 ];
