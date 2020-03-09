22cc79e34c55b666cc20cfdc5fafb60bcf055b27
Conner Shumway <ckshum16@stlawu.edu>
2020-03-09 14:12:34 -0400

    repeat ejs procedure for pies

--- express/bakery/pies.js
+++ express/bakery/pies.js
@@ -4,26 +4,27 @@ const express = require('express');
 // Create the Router
 const router = express.Router();
 
+// Pretend database collection
+const cakes = [
+  {id: 'apple', flavor: 'Apple'},
+  {id: 'cherry', flavor: 'Cherry'}
+];
+
 // cs-linuxlab-39.stlawu.edu:3000/pies/
 router.get('/', function(request, response) {
-  response.send(`
-    <h1>Pies</h1>
-    <ul>
-      <li><a href="/cakes/apple">Apple</a></li>
-      <li><a href="/cakes/cherry">Cherry</a></li>
-    </ul>
-  `);
+  response.render('pies/index', {pies: pies}));
 });
 
 // cs-linuslab-39.stlawu.edu:3000/pies/id
-router.get('/id', function(request, response) {
-  if (request.params.id === 'apple') {
-    response.send('Apple Pies');
-  } else if (request.params.id === 'cherry') {
-    response.send('Cherry Pies');
+router.get('/:id', function(request, response, next) {
+
+  // Pretend database lookup
+  const pie = pies.find(pie => pie.id === request.params.id);
+
+  if (!pie) {
+    next(); // Pass on this request
   } else {
-    next();  // Pass on this request
+    response.render('pie/detail', {pie: pie});
   }
 });
-
 module.exports = router;
