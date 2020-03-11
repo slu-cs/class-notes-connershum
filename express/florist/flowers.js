05312d45696dd33d27995bcae1cebd3c72bdd38f
Conner Shumway <ckshum16@stlawu.edu>
2020-03-11 13:48:42 -0400

    REST API for AJAX

--- express/florist/flowers.js
+++ express/florist/flowers.js
@@ -4,5 +4,62 @@ const express = require('express');
 // Create the server
 const router = express.Router();
 
+// Pretend database
+let flowers = [
+  {id: 'tulip', color: 'pink', season: 'spring'},
+  {id: 'rose', color: 'red', season: 'summer'}
+];
+
+// GET /flowers
+router.get('/', function(request, response) {
+  response.send(flowers);
+});
+
+// GET /flowers/id
+router.get('/:id', function(request, response, next) {
+  const flower = flowers.find(f => f.id === request.params.id);
+  if (!flower) {
+    next(); // Leads to 404
+  } else {
+    response.send(flower);
+  }
+});
+
+// POST /flowers
+router.post('/', function(request, response) {
+  const flower = request.body;
+  if (flowers.find(f => i.id === flower.id)) {
+    response.status(400).send('Duplicate');
+  } else if (flowers.find(f => f.id === flower.id)) {
+    response.status(400).send('Duplicate ID');
+  } else {
+    flowers.push(flower);
+    response.status(201).send(flowers);
+  }
+});
+
+// DELETE /flowers/id
+router.delete('/:id', function(request, response, next) {
+  const flower = flowers.find(f => f.id === request.params.id);
+  if (!flower) {
+    next(); // Leads to 404
+  } else {
+    flowers = flowers.filter(f => f.id !== flowers.id);
+    response.status(200).send(flowers);
+  }
+});
+
+// PUT /flower/id
+router.put('/:id', function(request, response, next) {
+  const flower = flowers.find(f => f.id === request.params.id);
+  if (!flower) {
+    next(); // Leads to 404
+  } else {
+    if (request.body.color) flower.color = request.body.color;
+    if (request.body.season) flower.season = request.body.season;
+    response.status(200).send(flowers);
+  }
+});
+
 // Use a view engine
 module.exports = router;
