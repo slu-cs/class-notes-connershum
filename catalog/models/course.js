2565a3373fc48a0f69bce38397cc64386d420ab6
connershum <connershum@gmail.com>
2020-03-23 12:43:10 -0400

    course and section schemas

---
+++ catalog/models/course.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Define the schema
+const Course = new mongoose.Schema({
+  _id: String,
+  title: String,
+  description: String,
+  prereqs: [String]
+});
+
+// Export the model
+module.exports = mongoose.model('Course', Course);
